7936f01c5bd516411f2c71c019ae747d32406334
Franco <frcg2424@hotmail.com>
2022-09-07 18:34:58 -0500

    feat: spring configuration

---
+++ src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sureservice_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SureServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SureServiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sureservice_backend/SureServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sureservice_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SureServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
