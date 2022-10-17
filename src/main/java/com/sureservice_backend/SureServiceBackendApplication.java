2a24b3dbc580eb1db330ac4f6fec5d175ad9541b
Williamz21 <u201816224@upc.edu.pe>
2022-10-16 19:36:59 -0500

    enabled CORS

--- src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
+++ src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
@@ -6,7 +6,10 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SecurityScheme(name = "vitec", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition
@@ -18,4 +21,14 @@ public class SureServiceBackendApplication {
 		SpringApplication.run(SureServiceBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+			}
+		};
+	}
 }
