e0ce1073926c1b517e20bf5d191370045457d963
Franco <frcg2424@hotmail.com>
2022-10-24 22:07:59 -0500

    CORS FIX

--- src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
+++ src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
@@ -27,7 +27,10 @@ public class SureServiceBackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+						.allowCredentials(true)
+						.allowedOrigins("*")
+						.allowedHeaders("*");
 			}
 		};
 	}
