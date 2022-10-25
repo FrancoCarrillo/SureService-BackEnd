3b331d074357c8c6cb4019ec6770217701093786
Franco <frcg2424@hotmail.com>
2022-10-24 22:03:59 -0500

    CORS FIX

--- src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
+++ src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
@@ -25,28 +25,19 @@ public class SureServiceBackendApplication {
 	}
 
 	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("DELETE");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
-
-	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+						.allowCredentials(true)
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("OPTIONS")
+						.allowedMethods("GET")
+						.allowedMethods("POST")
+						.allowedMethods("PUT")
+						.allowedMethods("DELETE");
 			}
 		};
 	}
