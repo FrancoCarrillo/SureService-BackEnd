5ee5de62f583ca129217a13fb4ea21baa819fb6e
Franco Carrillo <74079716+FrancoCarrillo@users.noreply.github.com>
2022-10-24 22:21:06 -0500

    Update SureServiceBackendApplication.java

--- src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
+++ src/main/java/com/sureservice_backend/SureServiceBackendApplication.java
@@ -27,7 +27,7 @@ public class SureServiceBackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
 						.allowCredentials(true)
 						.allowedOrigins("*")
 						.allowedHeaders("*");
